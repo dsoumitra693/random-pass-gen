093b4d3fb89846ab258a8998c254b71ef0c7f23c
Soumitra Das (Sam) <dsoumitra693@gmail.com>
2024-04-26 23:08:50 +0530

    able to adjust password length

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,26 @@ import './App.css'
 
 function App() {
   const [password, setPassword] = useState<string>()
+  const [passwordLength, setPasswordLength] = useState<number>(8)
 
   const handleInput = (evt: { target: { value: string | undefined } }) => {
     setPassword(evt.target.value)
   }
 
   const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_-+={[}];<,>./?|\/"
-  const generatePass = ()=>{
+  const generatePass = () => {
     let _str = str
     let _password = ""
-    let count = 8
-    while(count > 0){
-      _password += _str[~~(Math.random()*str.length)]
+    let count = passwordLength
+    while (count > 0) {
+      _password += _str[~~(Math.random() * str.length)]
       count--
     }
 
     setPassword(_password)
   }
 
-  const copyToClip = ()=>{
+  const copyToClip = () => {
     navigator.clipboard.writeText(password as string)
   }
 
@@ -34,6 +35,8 @@ function App() {
           <input value={password} onChange={handleInput} />
           <button onClick={copyToClip}>Copy</button>
         </div>
+        <label htmlFor="length">{passwordLength}</label>
+        <input value={passwordLength} id="length" className="secondary" type="range" name="vol" min="0" max="50" onChange={(e) => setPasswordLength(+e.target.value)} />
         <button onClick={generatePass}>Generate</button>
       </div>
     </div>
